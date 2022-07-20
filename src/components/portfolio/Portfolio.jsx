c81416508efe90bcf81152d2b36f68162219f275
BT <dinhhaudrm@gmail.com>
2022-07-20 19:55:39 +0700

    portfolio completed

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
 
 const Portfolio = () => {
   return (
@@ -7,8 +10,38 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
+        {/* -------- PROJECT 1 ------------- */}
         <article className='portfolio__item'>
-          
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+        {/* --------- PROJECT 2 ------------- */}
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG2} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+        {/* --------- PROJECT 3 ------------- */}
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG3} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
         </article>
       </div>
     </section>
