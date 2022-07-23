3f06feb6686a4937b115a85dcd692373907b663f
BT <dinhhaudrm@gmail.com>
2022-07-23 16:08:34 +0700

    completed project

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BsLinkedin } from 'react-icons/bs'
 import { BsGithub } from 'react-icons/bs'
-import { FiDribbble } from 'react-icons/fi'
+import { FaFacebookF } from 'react-icons/fa'
 
 const HeaderSocials = () => {
     return (
         <div className='header__socials'>
-            <a href="https://linkedin.com" target="_blank"><BsLinkedin /></a>
-            <a href="https://github.com" target="_blank"><BsGithub /></a>
-            <a href="https://dribbble.com" target="_blank"><FiDribbble /></a>
+            <a href="https://www.linkedin.com/in/haunguyen04/" target="_blank"><BsLinkedin /></a>
+            <a href="https://github.com/DinhHauNguyen" target="_blank"><BsGithub /></a>
+            <a href="https://www.facebook.com/dinhhau.nguyen.7" target="_blank"><FaFacebookF /></a>
         </div>
     )
 }
